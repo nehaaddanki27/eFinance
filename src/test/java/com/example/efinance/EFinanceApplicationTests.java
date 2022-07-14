19ddf7bcebec8dc2e11289f8f4fc54d23d553810
nehaaddanki27 <sarah.addanki@gmail.com>
2022-07-14 00:40:44 -0400

    Initial commit

---
+++ src/test/java/com/example/efinance/EFinanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.efinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFinanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
