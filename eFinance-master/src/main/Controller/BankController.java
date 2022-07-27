a004aa573614c75a86d1bde3a13f2f7a3f2b0a9c
Jesus Delapaz <JesusDelapaz2@live.com>
2022-07-27 17:33:23 -0400

    Add files via upload

---
+++ eFinance-master/src/main/Controller/BankController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BankController
+{
+    @RequestMapping("/banks")
+    @ResponseBody
+    public String index()
+    {
+        return "banks";
+    }
+}
