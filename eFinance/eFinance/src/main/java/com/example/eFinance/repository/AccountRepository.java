c2b9d7fd5848deaa06842b06b8dbfdeb2791bfef
Jesus Delapaz <JesusDelapaz2@live.com>
2022-07-31 21:45:45 -0400

    Add files via upload

--- eFinance/eFinance/src/main/java/com/example/eFinance/repository/AccountRepository.java
+++ eFinance/eFinance/src/main/java/com/example/eFinance/repository/AccountRepository.java
@@ -1,5 +1,10 @@
 package com.example.eFinance.repository;
 
-public interface AccountRepository
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.eFinance.model.Account;
+
+public interface AccountRepository extends JpaRepository<Account, Long>
 {
+
 }
--- eFinance/eFinance/src/main/java/com/example/eFinance/service/AccountService.java
+++ eFinance/eFinance/src/main/java/com/example/eFinance/service/AccountService.java
@@ -1,4 +1,20 @@
 package com.example.eFinance.service;
 
-public interface AccountService {
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.example.eFinance.model.Account;
+
+public interface AccountService
+{
+    List<Account> getAllAccounts();
+
+    void saveAccount(Account account);
+
+    Account getAccountById(long id);
+
+    void deleteAccountById(long id);
+
+    Page<Account> findPaginated(int pageNo, int pageSize, String sortField, String sortDirection);
 }
